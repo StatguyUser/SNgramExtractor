be811d93c6f2505614aad814ae536317325c1e43
newbiestatguy <azimulh@gmail.com>
2020-05-30 18:46:24 +0300

    Added module for SN-gram extraction

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import os
+import sys
+
+if sys.version_info[0] < 3:
+    with open('README.rst') as f:
+        long_description = f.read()
+else:
+    with open('README.rst', encoding='utf-8') as f:
+        long_description = f.read()
+
+
+setup(
+    name='SNgramExtractor',
+    version='0.0.1',
+    description='Implementation of syntactic n-grams (sn-gram) extraction',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This is important!
+    author='StatguyUser',
+    url='https://github.com/StatguyUser/SNgramExtractor',
+    install_requires=['spacy','spacy[es_core_news_md]'],
+    download_url='https://github.com/StatguyUser/SNgramExtractor.git',
+    py_modules=["SNgramExtractor"],
+    package_dir={'':'src'},
+)
---
+++ src/SNgramExtractor.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[333]:
+
+
+import spacy
+nlp = spacy.load('en_core_web_sm')
+
+class SNgramExtractor:
+    '''
+    text:input text
+    meta_tag:Resultant bigram and trigram should be concatenated with part of speech tag('pos') or dependency tag('dep') or original SN-gram('original')
+    trigram_flag:if we need to include trigrams derived from SN-grams as well ('yes') or not ('no'). Default is 'yes'
+    '''
+    def __init__(self,text,meta_tag,trigram_flag='yes'):
+        self.text=text
+        self.meta_tag=meta_tag
+        self.trigram_flag=trigram_flag
+
+    def get_trigram_element(self,trigram_element):
+        return '_'.join([str(element) for element in trigram_element.split('_')[:-1]])
+
+    def get_trigrams(self,left_right_words):
+        trigrams=[]
+        for i in range(len(left_right_words.values())):
+            right=list(left_right_words.values())[i]
+            right_value=list(left_right_words.keys())[i]
+            if right in left_right_words.keys():
+                #if present, find index and the actual word
+                left_indx=list(left_right_words.keys()).index(right)
+                #right,left key, left value
+                left_key=list(left_right_words.keys())[left_indx]
+                left_value=left_right_words[left_key]
+                trigrams.append(str(self.get_trigram_element(right_value))+'_'+str(self.get_trigram_element(left_key))+'_'+str(self.get_trigram_element(left_value)))
+        return ' '.join([str(trigram) for trigram in trigrams])
+    
+    def get_SNgram(self):
+        bigrams=[]
+        word_list=[]
+
+        left_right_words={}
+        unique_pos={}
+        unique_dep={}
+
+        nlp_obj=nlp(self.text)
+
+        for spacy_element in nlp_obj:
+            #no same head and body
+            if str(spacy_element.head)+str(spacy_element.head.idx)!=str(spacy_element)+str(spacy_element.idx):
+                ##check type of concatenation between head and body with meta attributes
+                if self.meta_tag=='dep':
+                    bigrams.append(str(spacy_element.head)+'_'+spacy_element.head.dep_+'_'+str(spacy_element)+'_'+spacy_element.dep_)
+                    left_right_words[str(spacy_element.head)+'_'+str(spacy_element.head.dep_)+'_'+str(spacy_element.head.idx)]=str(spacy_element)+'_'+str(spacy_element.dep_)+'_'+str(spacy_element.idx)
+                elif self.meta_tag=='pos':
+                    bigrams.append(str(spacy_element.head)+'_'+spacy_element.head.pos_+'_'+str(spacy_element)+'_'+spacy_element.pos_)
+                    left_right_words[str(spacy_element.head)+'_'+str(spacy_element.head.pos_)+'_'+str(spacy_element.head.idx)]=str(spacy_element)+'_'+str(spacy_element.pos_)+'_'+str(spacy_element.idx)
+                elif self.meta_tag=='original' or self.meta_tag=='':
+                    bigrams.append(str(spacy_element.head)+'_'+str(spacy_element))
+                    left_right_words[str(spacy_element.head)+'_'+str(spacy_element.head.idx)]=str(spacy_element)+'_'+str(spacy_element.idx)
+
+        flat_bigrams=' '.join([str(bigram) for bigram in bigrams])
+        
+        result_dict={}
+        result_dict['SNBigram']=flat_bigrams
+
+        if self.trigram_flag=='yes':
+            result_dict['SNTrigram']=self.get_trigrams(left_right_words)
+            return result_dict
+        else:
+            return result_dict
+        
+if __name__=="__main__":
+    
+    SNgram_obj=SNgramExtractor(text,meta_tag='original',trigram_flag='yes')
+    
+    text='Economic news have little effect on financial markets'
+    output=SNgram_obj.get_SNgram()    
+    print(text)
+    print('SNGram bigram:',output['SNBigram'])
+    print('SNGram trigram:',output['SNTrigram'])
+    
+    print('-----------------------------------')
+    text='every cloud has a silver lining'
+    output=SNgram_obj.get_SNgram()
+    print(text)
+    print('SNGram bigram:',output['SNBigram'])
+    print('SNGram trigram:',output['SNTrigram'])
+
